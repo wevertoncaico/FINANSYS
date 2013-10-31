e8d59cf65f649b6e10ae83a28bbfbb2b3fce2985
Monnalisa Medeiros <monna-mc@hotmail.com>
2013-10-31 14:28:38 -0300

    Criação da pagina inicial e adição de tema do primefaces
    obs: não forão enviadas a pasta lib( faltam as bibliotecas do primefaces 3.5 ;tema do primefaces blitzer-1.0.10 ; Derby ; todos os jar commons-io-2.4 , commons-fileupload-1.3; eclipselink ; eclipselinkmodelgen ; javaee-endorsed-api-6.0 ; jstl11 e jsf20
    
    Signed-off-by: Monnalisa Medeiros <monna-mc@hotmail.com>

---
+++ FINANSYS_Monna/src/java/modelo/Despesa.java
@@ -0,0 +1,85 @@
+/*
+ * To change this template, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package modelo;
+
+import java.io.Serializable;
+import java.util.Date;
+import javax.persistence.Entity;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+
+/**
+ *
+ * @author weverton
+ */
+@Entity
+public class Despesa implements Serializable {
+    private static final long serialVersionUID = 1L;
+    @Id
+    @GeneratedValue(strategy = GenerationType.AUTO)
+    private Long id;
+    private String descricao;
+    private double valor;
+    private Date data;
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+    
+    public String getDescricao() {
+        return descricao;
+    }
+
+    public void setDescricao(String descricao) {
+        this.descricao = descricao;
+    }
+
+    public double getValor() {
+        return valor;
+    }
+
+    public void setValor(double valor) {
+        this.valor = valor;
+    }
+
+    public Date getData() {
+        return data;
+    }
+
+    public void setData(Date data) {
+        this.data = data;
+    }
+
+    @Override
+    public int hashCode() {
+        int hash = 0;
+        hash += (id != null ? id.hashCode() : 0);
+        return hash;
+    }
+
+    @Override
+    public boolean equals(Object object) {
+        // TODO: Warning - this method won't work in the case the id fields are not set
+        if (!(object instanceof Despesa)) {
+            return false;
+        }
+        Despesa other = (Despesa) object;
+        if ((this.id == null && other.id != null) || (this.id != null && !this.id.equals(other.id))) {
+            return false;
+        }
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "modelo.Despesa[ id=" + id + " ]";
+    }
+    
+}
